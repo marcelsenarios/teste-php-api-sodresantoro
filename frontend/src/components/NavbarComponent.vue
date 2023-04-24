dd5312afb6c97b1548b086116ae941f833043fd2
Marcel Sena Rios <carlosmarcel@brq.com>
2023-04-24 18:01:24 -0300

    wip: criação frontend

---
+++ frontend/src/components/NavbarComponent.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div id="nav">
+        <router-link id="logo-url" to="/">
+            <img id="logo" :src="logo" :alt="alt">
+        </router-link>
+        <router-link to="/">Categorias</router-link>
+        <router-link to="/products">Produtos</router-link>
+        </div>
+    </div>
+</template>
+  
+  <script>
+  export default {
+    name: "NavbarComponent",
+    props: ["logo", "alt"]
+  }
+  </script>
+  
+  <style scoped>
+    #nav {
+      background-color: #e2e2e2;
+      border-bottom: 4px solid #111;
+      padding: 15px 50px;
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+    }
+    #nav #logo-url {
+      margin: auto;
+      margin-left: 0;
+    }
+    #logo {
+      width: 40px;
+      height: 40px;
+    }
+    #nav a {
+      color: #888;
+      text-decoration: none;
+      margin: 12px;
+      transition: .5s;
+    }
+    #nav a:hover {
+      color: #ccc;
+    }
+  </style>
